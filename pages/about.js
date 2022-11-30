ed7a9ba16a3c6b93422fcfb655e298daf7d46381
HeitorBaraldi <carlos.hgb88@gmail.com>
2022-11-30 14:35:59 -0300

    inserido prettier

--- pages/about.js
+++ pages/about.js
@@ -1,15 +1,15 @@
-import Image from "next/image"
-import styles from '../styles/About.module.css'
+import Image from "next/image";
+import styles from "../styles/About.module.css";
 
-export default function About(){
-      return (
-            <div className={styles.about}>
-                  <h1>Sobre o projeto</h1>
-                  <p>Projeto criado com NextJs do curso do Professor Matheus Battisti. 
-                        Utilizamos 2 Apis, 1 para pegar os dados dos pokemons, e a outra 
-                        para capturar as imagens.
-                  </p>
-                  <Image  src='/images/charizard.png' width='300' height='300' alt="Charizard"/>
-            </div>
-      )
+export default function About() {
+	return (
+		<div className={styles.about}>
+			<h1>Sobre o projeto</h1>
+			<p>
+				Projeto criado com NextJs do curso do Professor Matheus Battisti. Utilizamos 2
+				Apis, 1 para pegar os dados dos pokemons, e a outra para capturar as imagens.
+			</p>
+			<Image src="/images/charizard.png" width="300" height="300" alt="Charizard" />
+		</div>
+	);
 }
--- pages/pokemon/[pokemonId].js
+++ pages/pokemon/[pokemonId].js
@@ -1,79 +1,79 @@
-import Image from "next/image"
+import Image from "next/image";
+import styles from "../../styles/Pokemon.module.css";
+import { useRouter } from "next/router";
 
-import styles from '../../styles/Pokemon.module.css'
+export const getStaticPaths = async () => {
+	const maxPokemons = 251;
+	const api = "https://pokeapi.co/api/v2/pokemon/";
 
-import { useRouter } from "next/router"
+	const res = await fetch(`${api}/?limit=${maxPokemons}`);
+	const data = await res.json();
 
-export const getStaticPaths = async() => {
-      const maxPokemons = 251
-      const api = 'https://pokeapi.co/api/v2/pokemon/'
-    
-      const res = await fetch(`${api}/?limit=${maxPokemons}`)
-      const data = await res.json()
+	//params
+	const paths = data.results.map((pokemon, index) => {
+		return {
+			params: { pokemonId: (index + 1).toString() },
+		};
+	});
 
-      //params 
-      const paths = data.results.map((pokemon, index) => {
-            return {
-                  params: {pokemonId: (index+1).toString()},
-            }
-      })
+	return {
+		paths,
+		fallback: true,
+	};
+};
 
-      return {
-            paths,
-            fallback: true,
-      }
-}
+export const getStaticProps = async (context) => {
+	const id = context.params.pokemonId;
+	const res = await fetch(`https://pokeapi.co/api/v2/pokemon/${id}`);
+	const data = await res.json();
+	return {
+		props: { pokemon: data },
+	};
+};
 
-export const getStaticProps = async(context) => {
+export default function Pokemon({ pokemon }) {
+	const router = useRouter();
 
-      const id = context.params.pokemonId
-      const res = await fetch(`https://pokeapi.co/api/v2/pokemon/${id}`)
+	if (router.isFallback) {
+		return <div>Carregando...</div>;
+	}
 
-      const data = await res.json()
-
-      return {
-            props: {pokemon: data},
-      }
-}
-
-export default function Pokemon({pokemon}) {
-      const router = useRouter()
-
-      if (router.isFallback) {
-            return <div>Carregando...</div>
-      }
-
-      return (
-            <div className={styles.pokemon_container}>
-                  <h1 className={styles.title}>{pokemon.name}</h1>
-                  <Image
-                  src={`https://cdn.traction.one/pokedex/pokemon/${pokemon.id}.png`}
-                  width='200'
-                  height='200'
-                  alt={pokemon.name}
-                  />
-                  <div>
-                        <h3>Número:</h3>
-                        <p>#{pokemon.id}</p>
-                  </div>
-                  <div>
-                        <h3>Tipo:</h3>
-                        <div className={styles.types_container}>
-                              {pokemon.types.map((item, index) => (
-                                    <span key={index} className={`${styles.type} ${styles['type_' + item.type.name]}`}>{item.type.name}</span>
-                              ))}
-                        </div>
-                  </div>
-                  <div className={styles.data_container}>
-                        <div className={styles.data_height}>
-                              <h4>Altura: </h4>
-                              <p>{pokemon.height * 10} cm</p>
-                        </div>
-                        <div className={styles.data_weight}>
-                              <h4>Peso: </h4>
-                              <p>{pokemon.weight / 10} kg</p>
-                        </div>
-                  </div>
-            </div>
-            )
+	return (
+		<div className={styles.pokemon_container}>
+			<h1 className={styles.title}>{pokemon.name}</h1>
+			<Image
+				src={`https://cdn.traction.one/pokedex/pokemon/${pokemon.id}.png`}
+				width="200"
+				height="200"
+				alt={pokemon.name}
+			/>
+			<div>
+				<h3>Número:</h3>
+				<p>#{pokemon.id}</p>
+			</div>
+			<div>
+				<h3>Tipo:</h3>
+				<div className={styles.types_container}>
+					{pokemon.types.map((item, index) => (
+						<span
+							key={index}
+							className={`${styles.type} ${styles["type_" + item.type.name]}`}
+						>
+							{item.type.name}
+						</span>
+					))}
+				</div>
+			</div>
+			<div className={styles.data_container}>
+				<div className={styles.data_height}>
+					<h4>Altura: </h4>
+					<p>{pokemon.height * 10} cm</p>
+				</div>
+				<div className={styles.data_weight}>
+					<h4>Peso: </h4>
+					<p>{pokemon.weight / 10} kg</p>
+				</div>
+			</div>
+		</div>
+	);
 }
